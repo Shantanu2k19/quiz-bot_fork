7214a52440aac492261727a58a26178dddd2bae6
Shantanu2k19 <shantanusingh2030@gmail.com>
2025-01-15 18:04:57 +0530

    code added

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -15,7 +15,6 @@ def generate_bot_responses(message, session):
         return [error]
 
     next_question, next_question_id = get_next_question(current_question_id)
-
     if next_question:
         bot_responses.append(next_question)
     else:
@@ -32,6 +31,26 @@ def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to django session.
     '''
+    if not current_question_id:
+        return True, ""
+    
+    ans = 0
+    try: 
+        ans = int(answer)
+        if ans<0 or ans>4:
+            raise ValueError("Number range")
+    except:
+        print("wrong format")
+        return False, "Please input Number between 1 and 4"
+
+    ans_list = session.get('ans_list', [])
+
+    if(len(ans_list)==0):
+        ques_nos = len(PYTHON_QUESTION_LIST)
+        session['ans_list'] = [-1 for x in range(ques_nos+1)]
+        ans_list = session['ans_list']
+    
+    ans_list[current_question_id-2] = ans
     return True, ""
 
 
@@ -39,8 +58,22 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
+    if not current_question_id:
+        current_question_id=1
 
-    return "dummy question", -1
+    ques_nos = len(PYTHON_QUESTION_LIST)
+    if current_question_id>ques_nos:
+        return False, -1
+
+    ques = (f"Qn {str(current_question_id)} : ")
+    ques += PYTHON_QUESTION_LIST[current_question_id-1]["question_text"]
+    ques += "\n"
+    i=1
+    for op in PYTHON_QUESTION_LIST[current_question_id-1]["options"]:
+        ques += (f"\n Option {str(i)} => {op}")
+        i += 1
+
+    return ques, current_question_id+1
 
 
 def generate_final_response(session):
@@ -48,5 +81,32 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
+    ans_list = session['ans_list']
+    no_of_question = len(PYTHON_QUESTION_LIST)
+    correct_ans = []
+    final_result = "Thank you for taking quiz. \nHere is your result: \n"
 
-    return "dummy result"
+    for x in PYTHON_QUESTION_LIST:
+        option = x["options"]
+        ans = x["answer"]
+        try:
+            index = option.index(ans)
+            correct_ans.append(index+1)
+        except ValueError:
+            correct_ans.append(ans)
+
+    i=0
+    correct_ans_nos = 0
+
+    while(i<no_of_question):
+        final_result += (f" Qn. {i+1}: Ans: {correct_ans[i]}, \t Your Ans {ans_list[i]} \n")
+        
+        if correct_ans[i]==ans_list[i]:
+            correct_ans_nos+=1
+        i += 1
+
+    final_result+=(f"\nTotal: {no_of_question}, Correct: {correct_ans_nos} ({100*correct_ans_nos/no_of_question})%")
+    final_result+="\nEnter any key to try again!"
+
+
+    return final_result
